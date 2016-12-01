52c569aca198c984cfa0602be3d33c6bb816b537
Clear <wjddkdml1124@naver.com>
2016-12-01 22:01:48 +0900

    BST add

---
+++ Data_Structure_python/BST.py
@@ -0,0 +1,36 @@
+class Node(object):
+	def __init__(self,key):
+		self.key = key
+		self.left_child = None
+		self.right_child = None
+		self.parent = None 
+
+class BST(object):
+	# def insertNode():
+	# 	#
+#
+	def insert(self,key):
+		if this.search(key) == None:
+			insert_Node = Node(key)
+			if self.getRoot() == None:
+				root = insert_Node
+			else: insertNode(newnode,root)
+		def insertNode(self, newNode, currentNode):
+			if newNode.key > currentNode.key:
+				if currentNode.right_child == None:
+					currentNode.right_child = newNode
+					newNode.parent
+
+
+	def delete():
+
+	def search():
+		# key is in?
+
+	def printPreorder():
+
+	def printInorder():
+
+	def printPostorder():
+
+	def getRoot():
