19d3de856ab3deedb703b5cfe21508f66552a87b
Clear <wjddkdml1124@naver.com>
2017-01-11 15:52:55 +0900

    ListQueue Test main file

---
+++ Data_Structure_python/Queue/mainLQ.py
@@ -0,0 +1,10 @@
+from ListQueue import *
+
+if __name__ == '__main__':
+	LQ = ListQueue()
+	for x in range(10):
+		LQ.enqueue(x)
+		LQ.printqueue()
+	for _ in range(10):
+		LQ.dequeue()
+		LQ.printqueue()
